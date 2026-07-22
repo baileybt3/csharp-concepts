73414983f3ed8b6461517cff1db9b53f1d7ac248
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-07-21 22:44:51 -0400

    feat: add README files and 001-Comments

---
+++ Section 1 - Fundamentals/Introduction/002-Comments/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _002_Comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is an in-line comment
+            Console.WriteLine("Hello, World!");
+
+            /*
+             * This is a multi-line comment.
+             */
+
+        }
+    }
+}
