6ac5c01eb3a9e2438b5e339f3a06110e8d1d7d4f
baileybt3 <baileybt3@gmail.com>
2026-07-22 22:44:08 -0400

    feat: add 003-Numbers

---
+++ Section 1 - Fundamentals/Introduction/003-Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * - Basic Numbers
+ * - Brandon B
+ * - 07/22/2026
+ */
+
+int num1 = 25;
+int num2 = 50;
+
+int sum = num1 + num2;
+
+double num3 = 25.5;
+double num4 = 45.5;
+
+double sumDouble = num3 + num4;
+
+Console.Write("The sum of " + num1 + " and " + num2 + " is: " + sum + "\n");
+Console.Write("The sum of " + num3 + " and " + num4 + " is: " + sumDouble);
