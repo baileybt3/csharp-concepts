25fd3a9f3bfc7dd507498f16059b1f38624203d1
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-07-24 00:26:07 -0400

    feat: add 004-CharAndStrings

---
+++ Section 1 - Fundamentals/Introduction/004-CharAndStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _004_CharAndStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char aChar = 'B';
+            string aString = "This is a string made of chars";
+
+            Console.WriteLine(aChar);
+
+            Console.WriteLine("");
+
+            Console.WriteLine(aString);
+        }
+    }
+}
