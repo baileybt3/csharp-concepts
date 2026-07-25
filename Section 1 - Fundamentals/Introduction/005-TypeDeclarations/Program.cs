69221bb5f0e57b60286e570216b24251a2338ea7
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-07-25 14:32:28 -0400

    update: include constants and type casting

--- Section 1 - Fundamentals/Introduction/005-TypeDeclarations/Program.cs
+++ Section 1 - Fundamentals/Introduction/005-TypeDeclarations/Program.cs
@@ -10,6 +10,7 @@ namespace _005_TypeDeclarations
         static void Main(string[] args)
         {
             // Different Type Declarations
+            Console.WriteLine("--- Different Variable Declarations ---");
             int count = 10;
             double total = 5.25;
             char grade = 'A';
@@ -22,6 +23,23 @@ namespace _005_TypeDeclarations
             Console.WriteLine("Grade: " + grade);
             Console.WriteLine("Message: " + message);
             Console.WriteLine("Sent: " + sent);
+
+            // Declaring Constants
+            Console.WriteLine("\n--- Declaring Constants ---");
+            const int MAX_QUANTITY = 50;
+            const string COMPANY_NAME = "Coding Inc.";
+
+            Console.WriteLine("Maximum Quantity: " + MAX_QUANTITY);
+            Console.WriteLine("Company Name: " + COMPANY_NAME);
+
+            // Type Casting
+            Console.WriteLine("\n--- Type Casting ---");
+            double price = 9.99;
+            int intPrice = (int)price;
+
+            // Output 
+            Console.WriteLine("Price w/ Cents: " + price);
+            Console.WriteLine("Price w/o Cents: " + intPrice);
         }
     }
 }
