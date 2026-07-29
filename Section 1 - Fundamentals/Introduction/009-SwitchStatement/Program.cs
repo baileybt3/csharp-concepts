907fcd32e03fae2a8afba85232e9f7f6f1487996
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-07-28 21:20:06 -0400

    add: 009-SwitchStatement

---
+++ Section 1 - Fundamentals/Introduction/009-SwitchStatement/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Switch Statement Practice
+ * Brandon Bailey
+ * 07/28/2026
+ */
+namespace _009_SwitchStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = 0;
+            while(month < 1 || month > 12) {
+                Console.WriteLine("Enter the number of the month (1-12)");
+                
+                // Try parse string input
+                if(!int.TryParse(Console.ReadLine(), out month))
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    month = 0;
+                }
+            }
+
+            Console.WriteLine("Your month is: ");
+
+            switch (month) {
+
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+
+                case 2:
+                    Console.WriteLine("February");
+                    break;
+
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+
+                case 4:
+                    Console.WriteLine("April");
+                    break;
+
+                case 5:
+                    Console.WriteLine("May");
+                    break;
+
+                case 6:
+                    Console.WriteLine("June");
+                    break;
+
+                case 7:
+                    Console.WriteLine("July");
+                    break;
+
+                case 8:
+                    Console.WriteLine("August");
+                    break;
+
+                case 9:
+                    Console.WriteLine("September");
+                    break;
+
+                case 10:
+                    Console.WriteLine("October");
+                    break;
+
+                case 11:
+                    Console.WriteLine("November");
+                    break;
+
+                case 12:
+                    Console.WriteLine("December");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid month.");
+                    break;
+            }
+        }
+    }
+}
