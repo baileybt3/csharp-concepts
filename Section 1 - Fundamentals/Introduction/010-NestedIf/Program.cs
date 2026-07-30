a6860de2bb99886e6aed07ac77b9bc27ae1931c2
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-07-29 21:35:20 -0400

    add: 010-NestedIf

---
+++ Section 1 - Fundamentals/Introduction/010-NestedIf/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Nested If Statements
+ * Brandon Bailey
+ * 07/29/2026
+ */
+namespace _010_NestedIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int age = 20;
+            bool validId = true;
+            int height = 63;
+
+            string output = "";
+
+            // Can this person go to a bar?
+            if (height > 45)
+            {
+                if (validId)
+                {
+                    if (age >= 21)
+                    {
+                        output = "You can go in and drink.";
+                    }
+                    else
+                    {
+                        output = "You can go in but cannot drink.";
+                    }
+                }
+                else
+                {
+                    output = "Sorry, you need a valid ID.";
+                }
+            }
+            else
+            {
+                output = "Sorry, you are not tall enough.";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
