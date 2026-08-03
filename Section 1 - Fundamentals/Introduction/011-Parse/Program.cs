50a136cd5e6fbb76bdbd4c2a5bae96f9868cb93f
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-08-03 19:15:51 -0400

    add: 011-Parse

---
+++ Section 1 - Fundamentals/Introduction/011-Parse/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _011_Parse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = "2";
+            string secondString = "4";
+
+            int firstNumber = int.Parse(firstString);
+            int secondNumber = int.Parse(secondString);
+
+            Console.WriteLine(firstNumber + secondNumber);
+        }
+    }
+}
