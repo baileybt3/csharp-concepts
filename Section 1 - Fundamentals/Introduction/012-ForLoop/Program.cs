71cdb4cfb5c5eac83cd5597eef0c2a0c2ae1efe3
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-08-04 18:27:25 -0400

    add: 012-ForLoop

---
+++ Section 1 - Fundamentals/Introduction/012-ForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * For Loop
+ * 08/04/2026
+ * Brandon Bailey
+ */
+namespace _012_ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                sum += 1;
+                Console.WriteLine(sum);
+            }
+
+            Console.WriteLine($"Final Sum: {sum}");
+        }
+    }
+}
