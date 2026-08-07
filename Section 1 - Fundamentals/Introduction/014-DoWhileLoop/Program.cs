8bd3fc0ad38834c44e7965efa7ca9e8c6ef163a9
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-08-06 21:52:13 -0400

    add: 014-DoWhileLoop

---
+++ Section 1 - Fundamentals/Introduction/014-DoWhileLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Do-While Loop - Executes at least once before checking condition
+ * 08/06/2026
+ * Brandon Bailey
+ */
+
+int sum = 0;
+int num = 1;
+int count = 0;
+
+do
+{
+    num *= 2;
+    sum = sum + num;
+    Console.WriteLine("The sum is: " + sum);
+
+    count++;
+} while (count < 10);
+
+Console.WriteLine();
+
+bool isFalse = false;
+
+do
+{
+    num *= 2;
+    sum = 0;
+    
+    Console.WriteLine("The sum is: " + sum);
+} while (isFalse == true);
+
+
+
