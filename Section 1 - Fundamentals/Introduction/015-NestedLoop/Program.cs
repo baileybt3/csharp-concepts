c01adc54738c093ec3543d9ce685570a2fd7b4c7
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-08-11 19:06:22 -0400

    add: 015-NestedLoop

---
+++ Section 1 - Fundamentals/Introduction/015-NestedLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * 015 - Nested Loops
+ * Brandon Bailey
+ * 08/11/2026
+ */
+Console.WriteLine("Please enter a width: ");
+int width = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter a height: ");
+int height = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
