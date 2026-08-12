6ed69f46c2ad3e1d75814003d7a2d59a742b6b36
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-08-12 19:23:14 -0400

    add: 016-Methods and restructure folders

---
+++ Section 1 - Fundamentals/016-Methods/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * 016 - Methods
+ * Brandon Bailey
+ * 08/12/2026
+ */
+
+Console.Write("Enter the target number: ");
+int userNum = int.Parse(Console.ReadLine());
+
+int[] sumList = new int[userNum];
+
+Console.WriteLine($"\nFinal Sum: {GetSum(userNum, sumList)}");
+
+int GetSum(int userNum, int[] sumList)
+{
+    int sum = 0;
+
+    for (int i = 0; i < userNum; i++)
+    {
+        sumList[i] = i + 1;
+    }
+
+    for (int j = 0; j < sumList.Length; j++)
+    {
+        Console.Write(sumList[j] + " ");
+
+        int currNum = sumList[j];
+        sum += currNum;
+
+        Console.WriteLine($"Sum: {sum}");
+    }
+
+    return sum;
+
+}
+
