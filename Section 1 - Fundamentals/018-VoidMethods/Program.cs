d491d60db5699954034eb81ab325276d8f4ba45b
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-08-17 20:52:57 -0400

    add: 018-VoidMethods

---
+++ Section 1 - Fundamentals/018-VoidMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * 018 - Void Methods
+ * Brandon Bailey
+ * 08/17/2026
+ */
+namespace _018_VoidMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a comma seperated input!");
+
+            string userInput = Console.ReadLine();
+
+            countWords(userInput);
+
+        }
+
+        public static void countWords(string text)
+        {
+            string[] words = text.Split(',', StringSplitOptions.TrimEntries);
+
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
