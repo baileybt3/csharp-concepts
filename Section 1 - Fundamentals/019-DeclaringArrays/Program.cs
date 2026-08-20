514bdc8ddc20c393f573442643c3eaeba9b72fa1
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-08-19 21:03:21 -0400

    add: 019-DeclaringArrays

---
+++ Section 1 - Fundamentals/019-DeclaringArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Declaring Arrays
+ * 08/19/2026
+ * Brandon Bailey
+ */
+namespace _019_DeclaringArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] myStrings = new string[3];
+
+            Console.WriteLine("Enter 3 different strings");
+
+            Console.Write("Enter string 1: ");
+
+            myStrings[0] = Console.ReadLine();
+
+            Console.Write("Enter string 2: ");
+
+            myStrings[1] = Console.ReadLine();
+
+            Console.Write("Enter string 3: ");
+
+            myStrings[2] = Console.ReadLine();
+
+            Console.WriteLine("");
+            for(int i = 0; i < myStrings.Length; i++)
+            {
+                Console.WriteLine($"String {i + 1}: {myStrings[i]}");
+            }
+
+        }
+    }
+}
