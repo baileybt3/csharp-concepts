18055f0b5e4489999d92f48a7dbd71049c5b309f
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-08-30 20:14:44 -0400

    feat: 025-StringFormatting

---
+++ Section 1 - Fundamentals/025-StringFormatting/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _025_StringFormatting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your weight (lbs): ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine(FormatString(name, age, weight));
+
+        }
+
+        public static string FormatString(string name, int age, double weight)
+        {
+            string newName = name.Substring(0, 1).ToUpper() + name.Substring(1);
+
+            double dogYears = age * 7;
+
+            decimal newWeight = Convert.ToDecimal(weight / 14);
+
+            return $"Name: {newName}, Age in Dog Years: {dogYears}, Weight: {newWeight}";
+        }
+    }
+}
