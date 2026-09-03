ae0f9cda6f5ff268a45a3b1aa82c1bdcec154fdf
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-09-02 23:19:00 -0400

    fix: 027-ForEachLoop

--- Section 1 - Fundamentals/027-ForEachLoop/Program.cs
+++ Section 1 - Fundamentals/027-ForEachLoop/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace _027_ForEachLoop
+﻿/*
+ * 09/02/2026
+ * 027_ForEachLoop
+ * Brandon Bailey
+ */
+namespace _027_ForEachLoop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter a list of words separated by commas:");
+
+            string[] pokemonList = Console.ReadLine().Split(",");
+
+            foreach(string word in pokemonList)
+            {
+                Console.WriteLine(word);
+            }
+
         }
     }
 }
