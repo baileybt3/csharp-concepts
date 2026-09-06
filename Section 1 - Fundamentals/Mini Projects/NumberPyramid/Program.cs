ecbc71e145ddf73030e105b5c849ad1fd3f57e19
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-09-06 15:23:34 -0400

    feat: Mini Project - NumberPyramid

---
+++ Section 1 - Fundamentals/Mini Projects/NumberPyramid/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Number Pyramids
+ * Brandon Bailey
+ * 09/06/2026
+ */
+ namespace NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of your pyramid base: ");
+
+            // TryParse input to valid it is a positive integer
+            if (!int.TryParse(Console.ReadLine(), out int rowSize) || rowSize <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer.");
+                return;
+            }
+
+            // Loop through rows to create the pyramid
+            for (int row = 0; row < rowSize; row++)
+            {
+                int numberOfSpaces = rowSize - row - 1;
+                int numberOfStars = 2 * row + 1;
+
+                string spaces = new string(' ', numberOfSpaces);
+                string stars = new string('*', numberOfStars);
+
+                Console.WriteLine(spaces + stars);
+            }
+
+        }
+    }
+}
