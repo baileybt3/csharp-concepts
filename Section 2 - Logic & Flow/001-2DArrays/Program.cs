a375790a1fd619fce652c0c2b7ebe1e2a8c4bd31
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-09-08 18:16:21 -0400

    feat: section 2 001-2DArrays

---
+++ Section 2 - Logic & Flow/001-2DArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+   0-1-2D Arrays
+   Brandon Bailey
+   2024-06-10 
+*/
+namespace _001_2DArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] numberGrid =
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            // Access one value
+            Console.WriteLine($"Value at row 1, column 2: {numberGrid[1, 2]}");
+
+            Console.WriteLine("\nEntire Grid:");
+            // Display entire grid w nested loops
+            for(int row = 0; row < numberGrid.GetLength(0); row++)
+            {
+                for (int column = 0; column < numberGrid.GetLength(1); column++)
+                {
+                    Console.Write(numberGrid[row, column] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
